8285eb5983437959865c38420e1b43e4351705b9
NathanaelDelfino <nathanael.delfino@live.com>
2022-06-01 19:19:01 -0300

    :up: Adicionado novas validações

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -9,15 +9,33 @@ namespace PaymentContext.Tests;
 public class StudentTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void ShouldReturnsSucessWhereStudentIsValid()
     {
+        //Arrange
         var subscription = new Subscription(System.DateTime.Now);
         var name = new Name("Nathanael", "Delfino");
         var document = new Document("00000000000", EDocumentType.CPF);
         var email = new Email("nathan@gmail.com");
-        var address = new Address("","","","","","","");
-        var student = new Student(name, document,email, address);
-        
-        
+        var address = new Address("", "", "", "", "", "", "");
+        var student = new Student(name, document, email, address);
+        //Act
+        var retorno = student.Notifications;
+        //Assert
+        Assert.IsTrue(retorno.Count == 0);
+    }
+    [TestMethod]
+    public void ShouldReturnsErrorWhereStudentIsValid()
+    {
+        //Arrange
+        var subscription = new Subscription(System.DateTime.Now);
+        var name = new Name("", "Delfino");
+        var document = new Document("00000000000", EDocumentType.CPF);
+        var email = new Email("nathan@gmail.com");
+        var address = new Address("", "", "", "", "", "", "");
+        var student = new Student(name, document, email, address);
+        //Act
+        var retorno = student.Notifications;
+        //Assert
+        Assert.IsFalse(retorno.Count == 0);
     }
 }
